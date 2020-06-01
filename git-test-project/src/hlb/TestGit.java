8e465fba94a283e00e109b206e54e746988b2612
hlbTest <test>
2020-06-01 15:07:34 +0800

    添加测试git工程

---
+++ git-test-project/src/hlb/TestGit.java
@@ -0,0 +1,16 @@
+package hlb;
+
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("test git");
+        System.out.println("test git");
+        System.out.println("test git");
+        System.out.println("test git");
+    }
+
+    public void sum() {
+        int i = 0;
+        int j = i + 10;
+    }
+}
